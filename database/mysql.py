dae404cca4f08894660b4947eb26fed5f5ade44e
Danil <Danholder2003@yandex.com>
2024-08-08 09:51:39 +0500

    Update mysql.py

--- database/mysql.py
+++ database/mysql.py
@@ -1,105 +1,128 @@
+import aiomysql
+import asyncio
 import os
-import asyncmy
-from utils.logger import ModuleLogger
-from sqlalchemy import create_engine, inspect
-from sqlalchemy.orm import sessionmaker
-from fastapi import Depends
-from utils.config import ConfigUtil
-from models.db_models import Base, Customer, User
+from dotenv import load_dotenv
+from sshtunnel import SSHTunnelForwarder
+import logging
 
-logger = ModuleLogger(__name__).get_logger()
+load_dotenv()
 
-class MysqlClient:
+class MySQLDatabase:
     _instance = None
+    _lock = asyncio.Lock()
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(MysqlClient, cls).__new__(cls)
-            cls._instance._initialize()
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
         return cls._instance
 
-    def _initialize(self):
-        if not hasattr(self, '_initialized'):
-            self._initialized = True
-            self.config_util = ConfigUtil()
-            self.host = self.config_util.get_config('MYSQL', 'MYSQL_HOST')
-            self.port = int(self.config_util.get_config('MYSQL', 'MYSQL_PORT'))
-            self.user = self.config_util.get_config('MYSQL', 'MYSQL_USER')
-            self.password = self.config_util.get_config('MYSQL', 'MYSQL_PASSWORD')
-            self.db_name = self.config_util.get_config('MYSQL', 'MYSQL_DB_NAME')
-            self.pool = None
-            self.engine = None
+    async def __init__(self):
+        if not hasattr(self, "initialized"):
+            await self.connect()
+            self.initialized = True
 
-    async def create_pool(self):
-        self.pool = await asyncmy.create_pool(
-            host=self.host,
-            port=int(self.port),
-            user=self.user,
-            password=self.password,
-            db=self.db_name,
-            autocommit=True,
-            connect_timeout=10
-        )
-        logger.info("Try to connect to database: %s", self.db_name)
+    async def connect(self):
         try:
-            self.engine = create_engine(
-                f'mysql+asyncmy://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}'
-            )
-            if self.engine:
-                logger.info("Created connection engine",
-                            extra={"host": self.host, "port": self.port, "user": self.user, "db": self.db_name})
-                self.Session = sessionmaker(bind=self.engine)
-                logger.info("Created session maker...")
-                
-                inspector = inspect(self.engine)
-                existing_tables = inspector.get_table_names()
-                if not existing_tables:
-                    Base.metadata.create_all(bind=self.engine)
-                    logger.info("Created new tables")
+            db_host = os.getenv("MYSQL_HOST")
+            db_port = int(os.getenv("MYSQL_PORT", 3306))
+            db_user = os.getenv("MYSQL_USER")
+            db_password = os.getenv("MYSQL_PASSWORD")
+            db_database = os.getenv("MYSQL_DB")
 
+            vidation_db_host = os.getenv("MYSQL_HOST_VIDATION_SERVICE")
+            vidation_db_port = int(os.getenv("MYSQL_PORT_VIDATION_SERVICE", 3306))
+            vidation_db_user = os.getenv("MYSQL_USER_VIDATION_SERVICE")
+            vidation_db_password = os.getenv("MYSQL_PASSWORD_VIDATION_SERVICE")
+            vidation_db_database = os.getenv("MYSQL_DB_VIDATION_SERVICE")
+
+            use_ssh = os.getenv("USE_SSH") == "True"
+            self.use_ssh = use_ssh
+
+            if use_ssh:
+                ssh_host = os.getenv("SSH_HOST")
+                ssh_port = int(os.getenv("SSH_PORT", 22))
+                ssh_user = os.getenv("SSH_USER")
+                ssh_password = os.getenv("SSH_PASSWORD")
+
+                self.ssh_tunnel = SSHTunnelForwarder(
+                    (ssh_host, ssh_port),
+                    ssh_username=ssh_user,
+                    ssh_password=ssh_password,
+                    remote_bind_address=(db_host, db_port)
+                )
+                self.ssh_tunnel.start()
+                local_bind_port = self.ssh_tunnel.local_bind_port
+                print(f"SSH tunnel established successfully: {local_bind_port}")
+
+                self.pool = await aiomysql.create_pool(
+                    host='127.0.0.1',
+                    port=local_bind_port,
+                    user=db_user,
+                    password=db_password,
+                    db=db_database,
+                    autocommit=True
+                )
+
+                self.vidation_pool = await aiomysql.create_pool(
+                    host='127.0.0.1',
+                    port=local_bind_port,
+                    user=vidation_db_user,
+                    password=vidation_db_password,
+                    db=vidation_db_database,
+                    autocommit=True
+                )
             else:
-                logger.error("Error creating connection engine.")
-        except Exception as e:
-            logger.error("Error connecting to database: %s", e)
-            raise
+                self.pool = await aiomysql.create_pool(
+                    host=db_host,
+                    port=db_port,
+                    user=db_user,
+                    password=db_password,
+                    db=db_database,
+                    autocommit=True
+                )
 
-    async def execute(self, query, args=None):
-        try:
-            async with self.pool.acquire() as conn:
-                async with conn.cursor() as cursor:
-                    await cursor.execute(query, args)
-                    await conn.commit()
-        except asyncmy.errors.OperationalError as e:
-            logger.error(f"Error: {e}")
-            logger.info("Reconnecting to the database...")
-            await self.create_pool()
-            return await self.fetch(query, args)
+                self.vidation_pool = await aiomysql.create_pool(
+                    host=vidation_db_host,
+                    port=vidation_db_port,
+                    user=vidation_db_user,
+                    password=vidation_db_password,
+                    db=vidation_db_database,
+                    autocommit=True
+                )
+            logging.info("Success connecting to database")
         except Exception as e:
-            logger.error(f"Error: {e}")
-            return []
-
-    async def fetch(self, query, args=None):
-        try:
-            async with self.pool.acquire() as conn:
-                async with conn.cursor() as cursor:
-                    await cursor.execute(query, args)
-                    return await cursor.fetchall()
-        except asyncmy.errors.OperationalError as e:
-            logger.error(f"Error: {e}")
-            logger.info("Reconnecting to the database...")
-            await self.create_pool()
-            return await self.fetch(query, args)
-        except Exception as e:
-            logger.error(f"Error: {e}")
-            return []
+            logging.error(f"Error connecting to database: {e}")
+            raise e
 
     async def close(self):
-        if self.pool:
-            logger.info("Closing connection pool...")
-            self.pool.close()
-            logger.info("Waiting for connection pool to close...")
-            await self.pool.wait_closed()
+        self.pool.close()
+        self.vidation_pool.close()
+        if self.use_ssh:
+            self.ssh_tunnel.stop()
+        await self.pool.wait_closed()
+        await self.vidation_pool.wait_closed()
 
-def get_mysql_client():
-    client = MysqlClient()
-    return client
+    async def execute_query(self, query, params=None, as_dict=False, use_vidation_db=False):
+        cursor_type = aiomysql.DictCursor if as_dict else aiomysql.Cursor
+        pool = self.vidation_pool if use_vidation_db else self.pool
+
+        async with pool.acquire() as conn:
+            async with conn.cursor(cursor_type) as cur:
+                print("Executing SQL query:")
+                print("SQL:", cur.mogrify(query, params))
+                await cur.execute(query, params)
+                return await cur.fetchall()
+
+    async def fetch_one(self, query, params=None, as_dict=False, use_vidation_db=False):
+        return await self.execute_query(query, params, as_dict, use_vidation_db)
+
+    async def fetch_all(self, query, params=None, as_dict=False, use_vidation_db=False):
+        cursor_type = aiomysql.DictCursor if as_dict else aiomysql.Cursor
+        pool = self.vidation_pool if use_vidation_db else self.pool
+        async with pool.acquire() as conn:
+            async with conn.cursor(cursor_type) as cur:
+                await cur.execute(query, params)
+                return await cur.fetchall()
+
+
+database = MySQLDatabase()
